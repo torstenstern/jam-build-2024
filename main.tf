c3c8ef139de8ef231578138196bf38175ee50445
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-19 16:48:26 +0200

    update

--- main.tf
+++ main.tf
@@ -6,21 +6,21 @@ resource "aws_vpc" "vpc" {
 }
 
  resource "aws_vpc" "main_vpc" {
-   cidr_block = "10.0.0.0/16"
+   cidr_block = "10.1.0.0/24"
    tags = {
      "Name" = "CodeBuid-torsten-testhost"
      }
  }
 
-# Create an Internet Gateway
-resource "aws_internet_gateway" "main_igw" {
-  vpc_id = aws_vpc.main_vpc.id
-}
+# # Create an Internet Gateway
+# resource "aws_internet_gateway" "main_igw" {
+#   vpc_id = aws_vpc.main_vpc.id
+# }
 
 # Create a Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.1.0.0/25"
   availability_zone = var.region
   # map_public_ip_on_launch = true
 }
