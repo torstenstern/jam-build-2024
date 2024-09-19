df720022462f79bc92c3a514543dcbcebe60af78
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-19 16:46:57 +0200

    test

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = var.region
-  map_public_ip_on_launch = true
+  # map_public_ip_on_launch = true
 }
 
 #####################################
