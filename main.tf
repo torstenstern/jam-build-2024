fc32e988f1cdacfb33d07d18e36e50309abe7fd1
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-19 16:42:49 +0200

    update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+ resource "aws_vpc" "main_vpc" {
+   cidr_block = "10.0.0.0/16"
+   tags = {
+     "Name" = "CodeBuid-torsten-testhost"
+     }
+ }
+
 #####################################
 
 
