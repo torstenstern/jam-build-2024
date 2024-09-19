0c9803b7fd23f68558393c79865b5742e833a26a
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-19 17:40:25 +0200

    update AMI

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "allow_ssh" {
 
 # Create an EC2 Instance
 resource "aws_instance" "linux_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (Check your region for AMI ID)
+  ami           = "ami-033bb8199fdec0a84" # Amazon Linux 2 AMI (Check your region for AMI ID)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.allow_ssh.name]
