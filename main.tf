11d17592f5bc8971d5b5c8a96e5ee262ec9307dd
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-24 23:45:31 +0200

    i

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_security_group" "allow_ssh" {
 
 
 
-# Output the Public IP of the instance
-output "ec2_public_ip" {
-  value = aws_instance.linux_ec2.public_ip
-}
+# # Output the Public IP of the instance
+# output "ec2_public_ip" {
+#   value = aws_instance.linux_ec2.public_ip
+# }
