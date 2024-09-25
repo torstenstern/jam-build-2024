bd8787f2be7f9d6ef49af48f741327d34f5cb0f7
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 22:33:03 +0200

    update db

--- dynamodb.tf
+++ dynamodb.tf
@@ -16,7 +16,7 @@
 
 
 resource "aws_dynamodb_table" "my_table" {
-  name           = "my_table"
+  name           = var.dynamodb_table_name
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "flight_number"
   range_key      = "departure_date"
