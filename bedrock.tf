848e8d0b5f1a487d51340a76cd4d7f0e06fbfe44
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 13:42:38 +0200

    update

--- bedrock.tf
+++ bedrock.tf
@@ -62,7 +62,7 @@ resource "random_id" "bucket_suffix" {
 # Download the schema file from S3
 data "aws_s3_object" "schema_file" {
   bucket = aws_s3_bucket.bedrock_input_output_bucket.id
-  key    = "/input/api.yaml"
+  key    = "input/api.yaml"
 }
 
 resource "aws_iam_role" "cloudformation_role" {
