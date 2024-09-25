ed88d19f1592920fdc653e38821688d1fab7525d
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 17:38:50 +0200

    update

--- dynamodb.tf
+++ dynamodb.tf
@@ -2,7 +2,7 @@ resource "aws_dynamodb_table" "my_table" {
   name           = var.dynamodb_table_name
   billing_mode   = "PAY_PER_REQUEST"
 
-  hash_key       = "id"  # Primary key (adjust according to your schema)
+  hash_key       = "flight_number"  # Primary key (adjust according to your schema)
   
   attribute {
     name = "id"
