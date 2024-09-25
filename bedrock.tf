fff353778763d5dd1fa53ed2c4c16a4ea6aa8fbe
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 12:16:47 +0200

    test aws provider for bedrock

--- bedrock.tf
+++ bedrock.tf
@@ -76,34 +76,41 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
 
 
 
-resource "awscc_bedrock_agent" "example" {
-  agent_name              = "example-agent"
-  description             = "Example agent configuration"
-  agent_resource_role_arn = aws_iam_role.cloudformation_role.arn
-  # foundation_model        = "amazon.titan-text-gq-premier"
-  instruction             = "You are an office assistant in an insurance agency. You are friendly and polite. You help with managing insurance claims and coordinating pending paperwork."
-
-  idle_session_ttl_in_seconds = 600
-  auto_prepare                = true
-
-  # action_groups = [{
-  #   action_group_name = "example-action-group"
-  #   description       = "Example action group"
-  #   api_schema = {
-  #     s3 = {
-  #       s3_bucket_name = var.bucket_name
-  #       s3_object_key  = var.bucket_object_key
-  #     }
-    # }
-    # action_group_executor = {
-    #   lambda = var.lambda_arn
-    # }
-
-  # }]
-
-  tags = {
-    "Modified By" = "AWSCC"
-  }
-
+resource "aws_bedrockagent_agent" "example" {
+  agent_name                  = "my-agent-name"
+  agent_resource_role_arn     = aws_iam_role.cloudformation_role.arn
+  idle_session_ttl_in_seconds = 500
+  foundation_model            = "anthropic.claude-v2"
 }
+
+# resource "awscc_bedrock_agent" "example" {
+#   agent_name              = "example-agent"
+#   description             = "Example agent configuration"
+#   agent_resource_role_arn = aws_iam_role.cloudformation_role.arn
+#   foundation_model        = "amazon.titan-text-gq-premier"
+#   instruction             = "You are an office assistant in an insurance agency. You are friendly and polite. You help with managing insurance claims and coordinating pending paperwork."
+
+#   idle_session_ttl_in_seconds = 600
+#   auto_prepare                = true
+
+#   # action_groups = [{
+#   #   action_group_name = "example-action-group"
+#   #   description       = "Example action group"
+#   #   api_schema = {
+#   #     s3 = {
+#   #       s3_bucket_name = var.bucket_name
+#   #       s3_object_key  = var.bucket_object_key
+#   #     }
+#     # }
+#     # action_group_executor = {
+#     #   lambda = var.lambda_arn
+#     # }
+
+#   # }]
+
+#   tags = {
+#     "Modified By" = "AWSCC"
+#   }
+
+# }
 ################################################################################
