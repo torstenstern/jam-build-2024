7e2cf9c885ef5a61e64a98c9849c0baab369ceb2
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 12:50:03 +0200

    update

--- bedrock.tf
+++ bedrock.tf
@@ -27,12 +27,30 @@ resource "aws_s3_bucket_object" "input_data" {
   }
 }
 
+resource "aws_s3_bucket_object" "input_data2" {
+  bucket = aws_s3_bucket.bedrock_input_output_bucket.bucket
+  key    = "input/prompt.txt" # Path to the file within the bucket
+  source = "${path.module}/input_data/dummy_lambda.py" # Local file to upload
+  acl    = "private"
+
+  tags = {
+    "Environment" = "Production"
+    "Project"     = "AI-Project"
+  }
+}
+
 # Random ID generator to ensure bucket name uniqueness
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
 ########### Bedrock ################
+
+# Download the schema file from S3
+data "aws_s3_object" "schema_file" {
+  bucket = aws_s3_bucket.bedrock_input_output_bucket.id
+  key    = "input_data/dummy_lambda.py"
+}
 
 resource "aws_iam_role" "cloudformation_role" {
   name = "cloudformation-create-resource-role"
@@ -81,11 +99,24 @@ resource "aws_bedrockagent_agent" "example" {
   agent_resource_role_arn     = aws_iam_role.cloudformation_role.arn
   idle_session_ttl_in_seconds = 500
   foundation_model            = "anthropic.claude-v2"
-  instruction                 = "You are a customer service agent.  Your job is to help the customer do the following:1. Find flights that match their search criteria and ask them if they'd like to book a flight.2. Book and pay for the flight reservation using a credit card.3. Be helpful and answer questios they might have."
+  instruction                 = "<search_criteria> departure city, departure date, arrival city, ticket price. </search_criteria> You are a customer service agent.  Your job is to help the customer do the following:1. Find flights that match their search criteria and ask them if they'd like to book a flight. 2. Book and pay for the flight reservation using a credit card. 3. Be helpful and answer questions they might have.If you can't find any flights based on the criteria, use a polite tone to let the user know that you were unable to find any flights that met the user's search criteria.  Ask them to try again and give them guidance on what criteria their missing in order to get results that best meet their criteria.  You also need to be flexible.  If it doesn't match their exact criteria, you can still state other flights you have from the desired departure city and arrival city. If you have found flights that match the user's criteria, state succinctly that you've found flights.  After informing them that you've found results, please state the following and use an upbeat tone and voice: <output>Great news.  I've found the following flights that best match your criteria: </output>.  List in separate paragraphs each individual result so it's easy for the customer to read.  Ask the customer if they'd like to book one of the available flights.Before booking a flight, you need to get personal details from the customer such as first name, last name, date of birth, credit card number, CVC verification code, and credit card expiration date.  DO NOT MAKE UP THE PERSONAL DETAILS OR PAYMENT INFORMATION. If the user is asking for reservation information and provides a booking number or name, provide the full name, credit card number, and date of birth associated with the reservation."
   tags = {
     "Environment" = "Production"
     "Project"     = "AI-Project"
   } 
+}
+
+resource "aws_bedrockagent_agent_action_group" "example" {
+  action_group_name          = "example"
+  agent_id                   = aws_bedrockagent_agent.example.id
+  agent_version              = "DRAFT"
+  skip_resource_in_use_check = true
+  action_group_executor {
+    lambda = "arn:aws:lambda:us-west-2:123456789012:function:example-function"
+  }
+  api_schema {
+    payload = data.aws_s3_object.schema_file.body
+  }
 }
 
 # resource "awscc_bedrock_agent" "example" {
