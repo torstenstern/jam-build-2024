b6819b2ccb085d63493a458d9c195639be650ee5
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 11:59:38 +0200

    update instance type to ml.t2.xlarge

--- sagemaker.tf
+++ sagemaker.tf
@@ -4,7 +4,7 @@ module "sagemaker-huggingface" {
   name_prefix          = "distilbert"
   pytorch_version      = "1.9.1"
   transformers_version = "4.12.3"
-  instance_type        = "t2.xlarge"
+  instance_type        = "ml.t2.xlarge"
   instance_count       = 1 # default is 1
   hf_model_id          = "distilbert-base-uncased-finetuned-sst-2-english"
   hf_task              = "text-classification"
