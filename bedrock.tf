ff18a9cd5f99f049e1c2223720f95359247a6daf
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 16:24:19 +0200

    test

--- bedrock.tf
+++ bedrock.tf
@@ -129,7 +129,7 @@ resource "aws_bedrockagent_agent_action_group" "example" {
   skip_resource_in_use_check = true
   
    action_group_executor {
-    lambda = aws_lambda_function.my_lambda.id
+    lambda = "arn:aws:lambda:us-east-1:913410190579:function:example-function"
     }
     # api_schema {
     #   s3 {
