10d7fb10b640e741a5ccde9957aa6039a35a4322
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 12:18:56 +0200

    update agent

--- bedrock.tf
+++ bedrock.tf
@@ -81,6 +81,11 @@ resource "aws_bedrockagent_agent" "example" {
   agent_resource_role_arn     = aws_iam_role.cloudformation_role.arn
   idle_session_ttl_in_seconds = 500
   foundation_model            = "anthropic.claude-v2"
+  instruction                 = "You are a helpful assistant."
+  tags = {
+    "Environment" = "Production"
+    "Project"     = "AI-Project"
+  } 
 }
 
 # resource "awscc_bedrock_agent" "example" {
