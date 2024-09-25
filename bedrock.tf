f5a706fed9743b8ede99e7505489e9d1179c5d2c
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 12:13:25 +0200

    test bedrock again

--- bedrock.tf
+++ bedrock.tf
@@ -76,34 +76,34 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
 
 
 
-# resource "awscc_bedrock_agent" "example" {
-#   agent_name              = "example-agent"
-#   description             = "Example agent configuration"
-#   agent_resource_role_arn = aws_iam_role.cloudformation_role.arn
-#   foundation_model        = "amazon.titan-text-gq-premier"
-#   instruction             = "You are an office assistant in an insurance agency. You are friendly and polite. You help with managing insurance claims and coordinating pending paperwork."
+resource "awscc_bedrock_agent" "example" {
+  agent_name              = "example-agent"
+  description             = "Example agent configuration"
+  agent_resource_role_arn = aws_iam_role.cloudformation_role.arn
+  # foundation_model        = "amazon.titan-text-gq-premier"
+  instruction             = "You are an office assistant in an insurance agency. You are friendly and polite. You help with managing insurance claims and coordinating pending paperwork."
 
-#   idle_session_ttl_in_seconds = 600
-#   auto_prepare                = true
+  idle_session_ttl_in_seconds = 600
+  auto_prepare                = true
 
-#   # action_groups = [{
-#   #   action_group_name = "example-action-group"
-#   #   description       = "Example action group"
-#   #   api_schema = {
-#   #     s3 = {
-#   #       s3_bucket_name = var.bucket_name
-#   #       s3_object_key  = var.bucket_object_key
-#   #     }
-#     # }
-#     # action_group_executor = {
-#     #   lambda = var.lambda_arn
-#     # }
+  # action_groups = [{
+  #   action_group_name = "example-action-group"
+  #   description       = "Example action group"
+  #   api_schema = {
+  #     s3 = {
+  #       s3_bucket_name = var.bucket_name
+  #       s3_object_key  = var.bucket_object_key
+  #     }
+    # }
+    # action_group_executor = {
+    #   lambda = var.lambda_arn
+    # }
 
-#   # }]
+  # }]
 
-#   tags = {
-#     "Modified By" = "AWSCC"
-#   }
+  tags = {
+    "Modified By" = "AWSCC"
+  }
 
-# }
+}
 ################################################################################
