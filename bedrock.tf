5ad703fee6f0ed5b74ae16a691d51e302a6789ea
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-25 12:59:13 +0200

    update

--- bedrock.tf
+++ bedrock.tf
@@ -46,11 +46,11 @@ resource "random_id" "bucket_suffix" {
 
 ########### Bedrock ################
 
-# Download the schema file from S3
-data "aws_s3_object" "schema_file" {
-  bucket = aws_s3_bucket.bedrock_input_output_bucket.id
-  key    = "input_data/dummy_lambda.py"
-}
+# # Download the schema file from S3
+# data "aws_s3_object" "schema_file" {
+#   bucket = aws_s3_bucket.bedrock_input_output_bucket.id
+#   key    = "input/dummy_lambda.py"
+# }
 
 resource "aws_iam_role" "cloudformation_role" {
   name = "cloudformation-create-resource-role"
@@ -106,47 +106,17 @@ resource "aws_bedrockagent_agent" "example" {
   } 
 }
 
-resource "aws_bedrockagent_agent_action_group" "example" {
-  action_group_name          = "example"
-  agent_id                   = aws_bedrockagent_agent.example.id
-  agent_version              = "DRAFT"
-  skip_resource_in_use_check = true
-  action_group_executor {
-    lambda = "arn:aws:lambda:us-west-2:123456789012:function:example-function"
-  }
-  api_schema {
-    payload = data.aws_s3_object.schema_file.body
-  }
-}
-
-# resource "awscc_bedrock_agent" "example" {
-#   agent_name              = "example-agent"
-#   description             = "Example agent configuration"
-#   agent_resource_role_arn = aws_iam_role.cloudformation_role.arn
-#   foundation_model        = "amazon.titan-text-gq-premier"
-#   instruction             = "You are an office assistant in an insurance agency. You are friendly and polite. You help with managing insurance claims and coordinating pending paperwork."
-
-#   idle_session_ttl_in_seconds = 600
-#   auto_prepare                = true
-
-#   # action_groups = [{
-#   #   action_group_name = "example-action-group"
-#   #   description       = "Example action group"
-#   #   api_schema = {
-#   #     s3 = {
-#   #       s3_bucket_name = var.bucket_name
-#   #       s3_object_key  = var.bucket_object_key
-#   #     }
-#     # }
-#     # action_group_executor = {
-#     #   lambda = var.lambda_arn
-#     # }
-
-#   # }]
-
-#   tags = {
-#     "Modified By" = "AWSCC"
+# resource "aws_bedrockagent_agent_action_group" "example" {
+#   action_group_name          = "example"
+#   agent_id                   = aws_bedrockagent_agent.example.id
+#   agent_version              = "DRAFT"
+#   skip_resource_in_use_check = true
+#   action_group_executor {
+#     lambda = "arn:aws:lambda:us-west-2:123456789012:function:example-function"
 #   }
-
+#   api_schema {
+#     payload = data.aws_s3_object.schema_file.body
+#   }
 # }
+
 ################################################################################
