67de732cf6c76cdc750f00b8f109c1f1c3989687
Torsten Stern <tostern@paloaltonetworks.com>
2024-09-27 12:09:32 +0200

    asda

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_route_table_association" "public_rt_assoc" {
 }
 
 resource "aws_route_table_association" "public_rt_assoc2" {
-  subnet_id      = aws_subnet.public_subnet2.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
